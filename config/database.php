a7b54cbbbd3387aaf4f346d9584700ba3fb5dfa0
Own Abbas <94720597+Habibichopra@users.noreply.github.com>
2026-01-07 09:26:35 +0100

    Modifica nome del database

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
-define("DB_NAME", "Registro-Elettronico");
+define("DB_NAME", "Portale-Studenti");
 
 define("DB_CHARSET", "utf8mb4");
 
