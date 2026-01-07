0d828960bc24b9e5a93c46e6e19880b3d30093fd
herrera05 <matteoroberto.2005@gmail.com>
2026-01-07 09:58:01 +0100

    definizione dei vari metodi

--- classi/User.php
+++ classi/User.php
@@ -11,6 +11,49 @@ class User {
 
     }
 
+    //registra un nuovo utente nel database
+    public function registra($username, $password, $email, $nome, $cognome, $ruolo, $matricola = null) {
+        
+    }
+
+    //effettua il login dell utente
+    public function login($username, $password) {
+    
+    }
+    
+    //effettua il logout dell'tente
+    public function logout() {
+    
+    }
+
+    //restituisce i dati dell'utente in base
+    public function getUserById($id) {
+
+    }
+
+    //aggiorna il profilo delutente
+    public function updateProfile($id, $dati){
+
+    }
+
+    //restituisce la lista di tutti i studenti
+    public function getAllStudents() {
+
+    }
+
+    //restituisce la lista di tutti i professori
+    public function getAllProfessori() {
+
+    }
+
+    //elimino utente in base al id
+    public function deleteUser($id) {
+
+    }
+
+
+
+
 }
 
 
