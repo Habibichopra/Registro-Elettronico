c7e84aed5f6d3c141dda22eb72266d87676df202
herrera05 <matteoroberto.2005@gmail.com>
2026-01-15 11:28:33 +0100

    implementazione metodo per creare un compito

--- classi/Compito.php
+++ classi/Compito.php
@@ -14,7 +14,28 @@ class Compito {
     
     //creazione di un nuovo compito
     public function creaCompito($corso_id, $titolo, $descrizione, $scadenza, $punti_max, $allegato = null) {
-    
+        $query = "INSERT INTO " . $this->nome_tabella . " 
+                  (corso_id, titolo, descrizione, data_scadenza, punti_max, allegato) 
+                  VALUES (:corso_id, :titolo, :descrizione, :scadenza, :punti_max, :allegato)";
+        
+        $stmt = $this->conn->prepare($query);
+
+        $titolo = htmlspecialchars(strip_tags($titolo));
+        $descrizione = htmlspecialchars(strip_tags($descrizione));
+        $scadenza = htmlspecialchars(strip_tags($scadenza));
+        $punti_max = htmlspecialchars(strip_tags($punti_max));
+
+        $stmt->bindParam(":corso_id", $corso_id);
+        $stmt->bindParam(":titolo", $titolo);
+        $stmt->bindParam(":descrizione", $descrizione);
+        $stmt->bindParam(":scadenza", $scadenza);
+        $stmt->bindParam(":punti_max", $punti_max);
+        $stmt->bindParam(":allegato", $allegato);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
     }
 
     //aggorna compito
