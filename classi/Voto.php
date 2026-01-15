6cc9a783e868a13bed7c86a267a1de960e0136e9
herrera05 <matteoroberto.2005@gmail.com>
2026-01-15 10:50:46 +0100

    dichiarazione metodi per la classe voti

--- classi/Voto.php
+++ classi/Voto.php
@@ -11,6 +11,41 @@ class Voto{
         $database = Database::getInstance();
         $this->conn = $database->getConnection();
     }
+
+    //inserimento voto
+    public function addVoto($studente_id, $corso_id, $tipo, $voto, $note) {
+
+    }
+
+    //aggiorna voto
+    public function aggiornaVoto($id, $voto, $note) {
+
+    }
+
+    //eliminazione del voto
+    public function eliminaVoto($id) {
+    
+    }
+
+    //get voti di un studente
+    public function getVotiByStudente($studente_id, $corso_id = null) {
+
+    }
+
+    //calcola media voti studente
+    public function calcolaMedia($studente_id, $corso_id = null) {
+    
+    }
+
+    //statistiche del corso
+    public function getStatisticheCorso($corso_id) {
+
+    }
+
+    //generazione pagella in formato CSV
+    public function generatePagella($studente_id) {
+    
+    }
 }
 
 ?>
