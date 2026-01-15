62decaf35614e7bf401e37b111d603ed523a238f
herrera05 <matteoroberto.2005@gmail.com>
2026-01-15 10:46:47 +0100

    dichiarazione variabili

--- classi/Voto.php
+++ classi/Voto.php
@@ -2,7 +2,10 @@
 
 require_once __DIR__ . '/Database.php';
 class Voto{
-    
+    private $conn;
+    private $nome_tabella = "voti";
+    private $tabella_corsi = "corsi";
+    private $tabella_users = "users";
 }
 
 ?>
