5ffe680803728abc430f2fb1cef6791358aa0101
herrera05 <matteoroberto.2005@gmail.com>
2026-01-22 09:28:38 +0100

    dichiarazione metodi necessari

--- classi/Consegna.php
+++ classi/Consegna.php
@@ -13,6 +13,40 @@ class Consegna {
         $this->conn = $database->getConnection();
     }
 
+    //cosnega del compito da parte del stusente
+    public function consegnaCompito($compito_id, $studente_id, $file, $note) {
+
+    }
+
+    //valutazione del compito da parte del professore
+    public function valutaConsegna($consegna_id, $voto, $feedback) {
+
+    }
+
+    //get consegna in base al id
+    public function getConsegnaById($id) {
+
+    }
+
+    //lista delle consegne per un compito specifico
+    public function getConsegneByCompito($compito_id) {
+
+    }
+
+    //lista delle consegne per uno studente specifico
+    public function getConsegneByStudente($studente_id) {
+
+    } 
+
+    //controllo se la consegna è in ritardo
+    public function checkRitardo($consegna_id) {
+
+    }
+
+    //download del file della consegna
+    public function downloadConsegna($id) {
+
+    }
 }
 
 ?>
