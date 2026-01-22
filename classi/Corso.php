6d37733bf17fde3cb4d554156972f88a8ea80ef8
herrera05 <matteoroberto.2005@gmail.com>
2026-01-22 09:10:42 +0100

    istanze classe corso

--- classi/Corso.php
+++ classi/Corso.php
@@ -2,7 +2,10 @@
 require_once __DIR__ . '/Database.php';
 
 class Corso {
-
+    private $conn;
+    private $nome_tabella = "corsi";
+    private $tabella_iscrizioni = "iscrizioni";
+    private $tabella_users = "users";
 }
 
 ?>
