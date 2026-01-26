ded4537399a94f49ffc8029eeb0f70370aafd2b5
Own Abbas <94720597+Habibichopra@users.noreply.github.com>
2026-01-26 11:59:30 +0100

    prendo id del studetne loggato

--- studente/compiti.php
+++ studente/compiti.php
@@ -7,4 +7,6 @@ require_once '../includes/session_check.php';
 require_once '../classes/Corso.php';
 require_once '../classes/Compito.php';
 require_once '../classes/Consegna.php';
+
+$studente_id = $_SESSION['user_id'];
 ?>
