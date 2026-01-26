9d7fb2b6109ed873987d388785236cb46ee5e7bd
Own Abbas <94720597+Habibichopra@users.noreply.github.com>
2026-01-26 15:25:10 +0100

    caricamento della user , creazione dell'istanza delloggetto user e recuperare l'id del user

--- studente/profilo.php
+++ studente/profilo.php
@@ -4,5 +4,9 @@ require_once '../config/config.php';
 
 $required_ruolo = 'studente';
 require_once '../inclusi/session_check.php';
+
+require_once '../classi/User.php';
+$userObj = new User();
+$user_id = $_SESSION['user_id'];
 ?>
 
