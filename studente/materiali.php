f03b97b3bc7c5be63125009462be2f9a5fa8d76f
Own Abbas <94720597+Habibichopra@users.noreply.github.com>
2026-01-26 13:34:37 +0100

    recupero id del utente e creazione istanze oggetti

--- studente/materiali.php
+++ studente/materiali.php
@@ -6,4 +6,8 @@ require_once '../inclusi/session_check.php';
 
 require_once '../classi/Corso.php';
 require_once '../classi/Materiale.php';
+
+$studente_id = $_SESSION['user_id'];
+$corsoObj = new Corso();
+$materialeObj = new Materiale();
 ?>
