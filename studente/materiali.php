63c1bccf8f9d8cf1b35c9ad52188baae27f53391
Own Abbas <94720597+Habibichopra@users.noreply.github.com>
2026-01-26 13:34:11 +0100

    inclusione di classi da utilizzare

--- studente/materiali.php
+++ studente/materiali.php
@@ -3,4 +3,7 @@ require_once '../config/config.php';
 
 $required_ruolo = 'studente';
 require_once '../inclusi/session_check.php';
+
+require_once '../classi/Corso.php';
+require_once '../classi/Materiale.php';
 ?>
