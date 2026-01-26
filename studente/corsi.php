5c862cb65bcd389fe6f1f1368b0ebeb94972e1a3
Own Abbas <94720597+Habibichopra@users.noreply.github.com>
2026-01-26 11:32:10 +0100

    istanze oggetto corso

--- studente/corsi.php
+++ studente/corsi.php
@@ -5,4 +5,7 @@ $required_ruolo = 'studente';
 require_once '../inclusi/session_check.php';
 
 require_once '../classes/Corso.php';
+
+$corsoObj = new Corso();
+$studente_id = $_SESSION['user_id'];
 ?>
