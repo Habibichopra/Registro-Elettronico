6b62e56b1513b908255e64b229a991924f85a367
Own Abbas <94720597+Habibichopra@users.noreply.github.com>
2026-01-26 17:26:55 +0100

    correzzione errrori

--- studente/compiti.php
+++ studente/compiti.php
@@ -4,9 +4,9 @@ require_once '../config/config.php';
 $required_ruolo = 'studente';
 require_once '../inclusi/session_check.php';
 
-require_once '../classsi/Corso.php';
-require_once '../classsi/Compito.php';
-require_once '../classsi/Consegna.php';
+require_once '../classi/Corso.php';
+require_once '../classi/Compito.php';
+require_once '../classi/Consegna.php';
 
 $studente_id = $_SESSION['user_id'];
 
@@ -162,7 +162,7 @@ include '../inclusi/nav.php';
                                         echo '<span class="segno-stato stato-pericolo">In Ritardo</span>';
                                         break;
                                     case 'mancante':
-                                        echo '<span class="segno-stato" stato-scaduto>Non Consegnato</span>';
+                                        echo '<span class="segno-stato stato-scaduto">Non Consegnato</span>';
                                         break;
                                 }
                                 ?>
