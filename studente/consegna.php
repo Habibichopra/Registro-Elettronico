4ac052a37954dc80604cbf400b7d6e5b03e77e47
Own Abbas <94720597+Habibichopra@users.noreply.github.com>
2026-01-26 14:23:55 +0100

    inclusione delle classi necessarie

--- studente/consegna.php
+++ studente/consegna.php
@@ -5,4 +5,7 @@ require_once '../config/config.php';
 $required_ruolo = 'studente';
 require_once '../inclusi/session_check.php';
 
+require_once '../classu/Compito.php';
+require_once '../classi/Consegna.php';
+
 ?>
