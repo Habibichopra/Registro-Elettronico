d73a7197968914b18c655b9823a8478a4beec02d
Own Abbas <94720597+Habibichopra@users.noreply.github.com>
2026-01-26 16:07:46 +0100

    le classi necessarie

--- studente/comunicazione.php
+++ studente/comunicazione.php
@@ -5,4 +5,8 @@ require_once '../config/config.php';
 $required_ruolo = 'studente';
 require_once '../inclusi/session_check.php';
 
+require_once '../classi/Comunicazione.php';
+require_once '../classi/User.php';
+require_once '../classi/Corso.php';
+
 ?>
