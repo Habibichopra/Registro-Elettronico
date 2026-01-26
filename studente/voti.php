09ca3df1e36c508a5b2c4298935a4f3029e3b983
Own Abbas <94720597+Habibichopra@users.noreply.github.com>
2026-01-26 12:43:07 +0100

    prendo id del utente loggato e creo istanze degli oggetti

--- studente/voti.php
+++ studente/voti.php
@@ -7,5 +7,7 @@ require_once '../inclusi/session_check.php';
 require_once '../classi/Voto.php';
 require_once '../classi/EsportatoreCSV.php';
 
-
+$studente_id = $_SESSION['user_id'];
+$votoObj = new Voto();
+$csvExporter = new EsportatoreCSV();
 ?>
