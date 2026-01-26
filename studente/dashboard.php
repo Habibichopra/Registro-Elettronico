c021e95ddb3e8558892cc998915eb21e259f4b33
Own Abbas <94720597+Habibichopra@users.noreply.github.com>
2026-01-26 12:06:48 +0100

    correzzione

--- studente/dashboard.php
+++ studente/dashboard.php
@@ -4,9 +4,9 @@ require_once '../config/config.php';
 $required_ruolo = 'studente';
 require_once '../inclusi/session_check.php';
 
-require_once '../classes/Compito.php';
-require_once '../classes/Voto.php';
-require_once '../classes/Corso.php';
+require_once '../classi/Compito.php';
+require_once '../classi/Voto.php';
+require_once '../classi/Corso.php';
 
 $studente_id = $_SESSION['user_id'];
 
