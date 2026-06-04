44c58f04c74d4875361409b3c6a696f7c10d1490
Own Abbas <94720597+Habibichopra@users.noreply.github.com>
2026-06-04 08:20:34 +0200

    Update del BASE_URL poiche la cartella scaricata da github appare con -main

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 define('SITE_NAME', 'Portale Studenti Universitario');
-define('BASE_URL', 'http://localhost/Portale-Studenti/');
+define('BASE_URL', 'http://localhost/Portale-Studenti-main/');
 
 //avvio la sessione s enon è gia ccesa
 if (session_status() === PHP_SESSION_NONE) {
